5e252ea5686dcf1907f587e3373bace0af656b35
Sandro B <s.badame@gmail.com>
2010-01-28 16:53:52 -0500

    - Added a build file (use target: test-run to run it and open the debugger on port 8000) - Added a test program that just reads a char from STDIN

---
+++ src/test/ReadChar.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ReadChar{
+    
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt());
+    }
+
+}
