d3b5bd2d9b52f9969b67c55a1da20db39ef9623c
sbadame <s.badame@gmail.com>
2010-01-28 23:00:28 -0500

    - Simple program that can actually connect to a running java program and print the loaded classes - update gitignore to ignore my netbeans project files - still working on making the build.xml more friendly

---
+++ src/main/memeograph/Memeographer.java
@@ -0,0 +1,56 @@
+package memeograph;
+
+import com.sun.jdi.Bootstrap;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.connect.AttachingConnector;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.Connector.Argument;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Starting off the begining of the Memeographer. This class starts the show.
+ * 
+ */
+public class Memeographer {
+	// We are assuming that the target vm was started with 'transport=dt_socket'
+	private static final String TRANSPORT = "com.sun.jdi.SocketAttach";
+
+	// We are also assuming that the target is waiting for us on port 8000
+	private static final String PORT = "8000";
+
+	public static void main(String[] args){
+		VirtualMachine vm = getTargetVM();
+		if (vm == null)
+			throw new Error("No VM was found");
+		System.out.println(vm.allClasses());
+	}
+
+	private static VirtualMachine getTargetVM(){
+		List connectors = Bootstrap.virtualMachineManager().attachingConnectors();
+		Iterator i = connectors.iterator();
+		while(i.hasNext()){
+			Connector c = (Connector)i.next();
+			System.out.println(c.name());
+			if (c.name().equals(TRANSPORT)) {
+				AttachingConnector ac = (AttachingConnector)c;
+				try {
+					Map<String, Argument> args = ac.defaultArguments();
+					args.get("port").setValue(PORT);
+					return ac.attach(args);
+				} catch (IOException ex) {
+					throw new Error("Unable to attach to target VM: " + ex);
+				} catch (IllegalConnectorArgumentsException ex) {
+					System.err.println("Bad Arguments: " + ex.argumentNames());
+					throw new Error("Bad Connector Arguments: " + ex);
+				}
+			}
+		}
+
+		return null;
+	}
+
+}
