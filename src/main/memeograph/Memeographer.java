6f00a0dd3046cb02483cfb264d90d1354db97e1c
sbadame <s.badame@gmail.com>
2010-01-30 14:43:31 -0500

    my netbeans project files are now online to ease working between environments, deleted README, changed Memeographer to create output.dot

--- src/main/memeograph/Memeographer.java
+++ src/main/memeograph/Memeographer.java
@@ -1,17 +1,26 @@
 package memeograph;
 
 import com.sun.jdi.Bootstrap;
+import com.sun.jdi.ClassType;
 import com.sun.jdi.ObjectReference;
 import com.sun.jdi.ReferenceType;
+import com.sun.jdi.Value;
 import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.connect.AttachingConnector;
 import com.sun.jdi.connect.Connector;
 import com.sun.jdi.connect.Connector.Argument;
 import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Starting off the begining of the Memeographer. This class starts the show.
@@ -24,12 +33,39 @@ public class Memeographer {
 	// We are also assuming that the target is waiting for us on port 8000
 	private static final String PORT = "8000";
 
+	private static HashMap<Long, ArrayList<Long>> hashMap = new HashMap<Long, ArrayList<Long>>();
+
 	public static void main(String[] args){
 		VirtualMachine vm = getTargetVM();
 		if (vm == null)
 			throw new Error("No VM was found");
 		for(ReferenceType t : vm.allClasses()){
-			System.out.println(t.name()+ " - " + t.instances(100).size());
+			searchType(t);
+		}
+		try {
+			PrintWriter out = new PrintWriter(new FileWriter("output.dot"), true);
+			out.println("graph memeograph {");
+			for (Long object : hashMap.keySet()) {
+				for (Long ref : hashMap.get(object)) {
+					out.println("  " + object + " -> " + ref + ";");
+				}
+			}
+			out.println("}");
+			out.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private static void searchType(ReferenceType t){
+		for (ObjectReference o : t.instances(0)) {
+			ArrayList<Long> list = new ArrayList<Long>();
+			for (Value val : o.getValues(t.allFields()).values() ){
+				if (val != null && val.type() != null && val.type() instanceof ClassType){
+					list.add(((ObjectReference)val).uniqueID());
+				}
+			}
+			hashMap.put(o.uniqueID(), list);
 		}
 	}
 
