4901f38fa0d55a4d36b661284ac7ce9d7f331b8b
Peter Boothe <peter.boothe@manhattan.edu>
2010-02-01 15:48:50 -0500

    Memeographer now successfully outputs a graph in digraph form.  ant build pdf for an example.

--- src/main/memeograph/Memeographer.java
+++ src/main/memeograph/Memeographer.java
@@ -1,11 +1,6 @@
 package memeograph;
 
-import com.sun.jdi.Bootstrap;
-import com.sun.jdi.ClassType;
-import com.sun.jdi.ObjectReference;
-import com.sun.jdi.ReferenceType;
-import com.sun.jdi.Value;
-import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.*;
 import com.sun.jdi.connect.AttachingConnector;
 import com.sun.jdi.connect.Connector;
 import com.sun.jdi.connect.Connector.Argument;
@@ -27,69 +22,108 @@ import java.util.logging.Logger;
  * 
  */
 public class Memeographer {
-	// We are assuming that the target vm was started with 'transport=dt_socket'
-	private static final String TRANSPORT = "com.sun.jdi.SocketAttach";
+    // We are assuming that the target vm was started with 'transport=dt_socket'
+    private static final String TRANSPORT = "com.sun.jdi.SocketAttach";
 
-	// We are also assuming that the target is waiting for us on port 8000
-	private static final String PORT = "8000";
+    // We are also assuming that the target is waiting for us on port 8000
+    private static final String PORT = "8000";
 
-	private static HashMap<Long, ArrayList<Long>> hashMap = new HashMap<Long, ArrayList<Long>>();
+    private static HashMap<String, ArrayList<String>> hashMap = new HashMap<String, ArrayList<String>>();
 
-	public static void main(String[] args){
-		VirtualMachine vm = getTargetVM();
-		if (vm == null)
-			throw new Error("No VM was found");
-		for(ReferenceType t : vm.allClasses()){
-			searchType(t);
-		}
-		try {
-			PrintWriter out = new PrintWriter(new FileWriter("output.dot"), true);
-			out.println("graph memeograph {");
-			for (Long object : hashMap.keySet()) {
-				for (Long ref : hashMap.get(object)) {
-					out.println("  " + object + " -> " + ref + ";");
-				}
-			}
-			out.println("}");
-			out.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
+    public static void main(String[] args) 
+    { 
+        VirtualMachine vm = getTargetVM();
+        if (vm == null)
+            throw new Error("No VM was found");
+        vm.suspend();
 
-	private static void searchType(ReferenceType t){
-		for (ObjectReference o : t.instances(0)) {
-			ArrayList<Long> list = new ArrayList<Long>();
-			for (Value val : o.getValues(t.allFields()).values() ){
-				if (val != null && val.type() != null && val.type() instanceof ClassType){
-					list.add(((ObjectReference)val).uniqueID());
-				}
-			}
-			hashMap.put(o.uniqueID(), list);
-		}
-	}
+        for(ReferenceType t : vm.allClasses()){
+            searchType(t);
+        }
 
-	private static VirtualMachine getTargetVM(){
-		List connectors = Bootstrap.virtualMachineManager().attachingConnectors();
-		Iterator i = connectors.iterator();
-		while(i.hasNext()){
-			Connector c = (Connector)i.next();
-			if (c.name().equals(TRANSPORT)) {
-				AttachingConnector ac = (AttachingConnector)c;
-				try {
-					Map<String, Argument> args = ac.defaultArguments();
-					args.get("port").setValue(PORT);
-					return ac.attach(args);
-				} catch (IOException ex) {
-					throw new Error("Unable to attach to target VM: " + ex);
-				} catch (IllegalConnectorArgumentsException ex) {
-					System.err.println("Bad Arguments: " + ex.argumentNames());
-					throw new Error("Bad Connector Arguments: " + ex);
-				}
-			}
-		}
+        for (ThreadReference tr : vm.allThreads()) {
+            int sfcount = 0;
+            try {
+                for (StackFrame sf : tr.frames()) {
+                    hashMap.put("StackFrame" + sfcount, new ArrayList<String>());
+                    if (sfcount > 0) {
+                        hashMap.get("StackFrame" + (sfcount-1)).add("StackFrame" + sfcount);
+                    }
 
-		return null;
-	}
+                    try {
+                        for (LocalVariable lv : sf.visibleVariables()) {
+                            Value val = sf.getValue(lv);
+                            if (val != null && val.type() != null && val.type() instanceof ClassType){
+                                ObjectReference or = (ObjectReference)val;
+                                hashMap.get("StackFrame" + sfcount).add("" + or.referenceType().name() + "<" + or.uniqueID() + ">");
+                            }
+                        }
+                    } catch (AbsentInformationException aie) {
+                        System.out.println("No such info for stack frame " + sfcount);
+                    }
+                    sfcount++;
+                }
+            } catch (IncompatibleThreadStateException itse) {
+                System.out.println("Why in the world do we have an IncompatibleThreadStateException?");
+                itse.printStackTrace();
+            }
+        }
+
+        for (ThreadReference tr : vm.allThreads()) tr.resume();
+
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter("output.dot"), true);
+            out.println("digraph memeograph {");
+            for (String object : hashMap.keySet()) {
+                for (String ref : hashMap.get(object)) {
+                    out.println("  \"" + object + "\" -> \"" + ref + "\";");
+                }
+            }
+            out.println("}");
+            out.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        vm.resume();
+    }
+
+    private static void searchType(ReferenceType t){
+        for (ObjectReference o : t.instances(0)) {
+            if (o.referenceType().name().startsWith("java.")) continue;
+            if (o.referenceType().name().startsWith("sun.")) continue;
+
+            ArrayList<String> list = new ArrayList<String>();
+            for (Value val : o.getValues(t.allFields()).values() ){
+                if (val != null && val.type() != null && val.type() instanceof ClassType){
+                    ObjectReference or = (ObjectReference)val;
+                    list.add("" + or.referenceType().name() + "<" + or.uniqueID() + ">");
+                }
+            }
+            hashMap.put("" + o.referenceType().name() + "<" + o.uniqueID() + ">", list);
+        }
+    }
+
+    private static VirtualMachine getTargetVM(){
+        List connectors = Bootstrap.virtualMachineManager().attachingConnectors();
+        Iterator i = connectors.iterator();
+        while(i.hasNext()){
+            Connector c = (Connector)i.next();
+            if (c.name().equals(TRANSPORT)) {
+                AttachingConnector ac = (AttachingConnector)c;
+                try {
+                    Map<String, Argument> args = ac.defaultArguments();
+                    args.get("port").setValue(PORT);
+                    return ac.attach(args);
+                } catch (IOException ex) {
+                    throw new Error("Unable to attach to target VM: " + ex);
+                } catch (IllegalConnectorArgumentsException ex) {
+                    System.err.println("Bad Arguments: " + ex.argumentNames());
+                    throw new Error("Bad Connector Arguments: " + ex);
+                }
+            }
+        }
+
+        return null;
+    }
 
 }
--- src/test/ReadChar.java
+++ src/test/ReadChar.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
 
 public class ReadChar{
+    class InnerClassName {
+        int i;
+    }
+
+    InnerClassName thing = new InnerClassName();
     
     public static void main(String[] args){
+        ReadChar r = new ReadChar();
+        System.out.println(r);
         Scanner s = new Scanner(System.in);
         System.out.println(s.nextInt());
+        System.out.println(r);
     }
 
 }
