33d5727808276cb346952ebc578602d72a4d07c0
sbadame <s.badame@gmail.com>
2010-03-28 11:53:51 -0400

    I think i'm on the right path

--- src/main/memeograph/ui/MemeoPApplet.java
+++ src/main/memeograph/ui/MemeoPApplet.java
@@ -23,8 +23,8 @@ public class MemeoPApplet extends PApplet implements TreeChangeListener, MouseWh
     static float FRICTION = .95f;
     static int MOVE_TICK = 50;
 
-    private Map<DiGraph, Node> positions;
-    private Vector<Vector<Node>> layers;
+    private Map<DiGraph, Node> positions = new HashMap<DiGraph, Node>();
+    private Vector<Vector<Vector<Node>>> layers  = new Vector<Vector<Vector<Node>>>();
 
     private DiGraph tree;
     private boolean treechanged = false;
@@ -75,7 +75,7 @@ public class MemeoPApplet extends PApplet implements TreeChangeListener, MouseWh
         //First check if we have to layout this stuff out
         if (!laidout) {
             treechanged = false;
-            layout(tree, width/2, PADDING);
+            layout(tree, width/2);
         }
 
         //jiggle our layout
@@ -120,46 +120,56 @@ public class MemeoPApplet extends PApplet implements TreeChangeListener, MouseWh
         translate(-(float)n.x, -(float)n.y, -(float)n.z);
     }
 
-    private void layout(DiGraph t, double x, double y){
-        positions = new HashMap<DiGraph,Node>();
-        layers = new Vector<Vector<Node>>();
-        layers.add(new Vector<Node>());
+    private void layout(DiGraph t, double x){
+        Vector<Vector<Node>> current_layer = new Vector<Vector<Node>>();
+        Vector<Node> current_row = new Vector<Node>();
+        current_layer.add(current_row);
 
-        Queue<DiGraph> curr_layer = new LinkedList<DiGraph>();
-        curr_layer.add(t);
+        Vector<Vector<Node>> next_layer = new Vector<Vector<Node>>();
+        Vector<Node> yrow = new Vector<Node>();
+        next_layer.add(yrow);
 
-        int layer = 0;
-        Queue<DiGraph> next_layer = new LinkedList<DiGraph>();
+        Vector<Node> zrow = new Vector<Node>();
+        current_layer.add(zrow);
+
+        layers.add(current_layer);
+        layers.add(next_layer);
+
+
+        Queue<DiGraph> zqueue = new LinkedList<DiGraph>();
+        Queue<DiGraph> yqueue = new LinkedList<DiGraph>();
+        Queue<DiGraph> xqueue = new LinkedList<DiGraph>();
+
+        xqueue.add(t);
 
         double xposition = x;
+        int yposition = 0;
+        int zposition = 0;
 
-        while (!curr_layer.isEmpty() || !next_layer.isEmpty()) {
-            if (curr_layer.isEmpty()) {
-                layer += 1;
-                curr_layer = next_layer;
-                next_layer = new LinkedList<DiGraph>();
-                layers.add(new Vector<Node>());
-                xposition = x;
+        while(!xqueue.isEmpty()){
+            DiGraph g = xqueue.remove();
+            for (DiGraph z : g.getDataChildren()) {
+                if (positions.containsKey(z)) continue;
+                zqueue.add(z);
             }
 
-            t = curr_layer.remove();
-            if (positions.containsKey(t)) continue;
-
-            t.addTreeChangeListener(this);
-
-            Node n = new Node(t, xposition, (layer+1)*50);
-            n.width = textWidth(n.data.getTreeName());
-            xposition += n.width + 100;
-
-            layers.get(layer).add(n);
-            positions.put(t, n);
-
-            for (DiGraph kid : t.getSoftwareChildren()) {
-                if (!positions.containsKey(kid)) {
-                    next_layer.add(kid);
-                }
+            for (DiGraph y : g.getSoftwareChildren()) {
+                if (positions.containsKey(y)) continue;
+                yqueue.add(y);
             }
+
+            Node node = new Node(g, xposition,(yposition + 1)*50, (zposition + 1) * 50 );
+            positions.put(g, node);
+            xposition += textWidth(g.getTreeName()) + 100;
         }
+
+        if (!zqueue.isEmpty()){
+            xqueue = zqueue;
+            zqueue = new LinkedList<DiGraph>();
+            //keep going!
+
+        }
+
 
         laidout = true; 
     }
